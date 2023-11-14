c3f4f4176faea84ce316a69362dae38b31899b8b
German Andrés Velasco Bossa <germana-velasco@javeriana.edu.co>
2023-11-14 17:25:28 -0500

    creacion2

--- application/src/main/java/com/example/demo/personaApp/application/port/out/PersonOutputPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.personaApp.application.port.out;
-
-@Port
-public interface PersonOutputPort {
-    public Person save(Person person);
-    public Boolean delete(Integer identification);
-    public List<Person> find();
-    public Person findById(Integer identification);
-}
